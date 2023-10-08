71251e58c59662017c37aab83648a2486d0f6791
Caitlin Colver <colvercm@gmail.com>
2023-10-08 10:31:00 -0600

    153 tests passed

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express')
 const apiRoutes = require('./routes/api-routes')
 const htmlRoutes = require('./routes/html-routes')
 const app = express()
+
 // TODO: Require Exphbs
+const exphbs = require('express-handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 // TODO: Add the handlebars setup code
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
+// Serve static 
 app.use(express.static('public'))
 
 app.use('/', htmlRoutes)
